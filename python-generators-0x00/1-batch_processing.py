33de0d3e403cdfa77e4cac2f5988ec8b0d07823f
shemmee.codes <56132945+s-shemmee@users.noreply.github.com>
2025-07-06 22:20:39 +0100

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -20,4 +20,4 @@ def batch_processing(batch_size):
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user["age"] > 25:
-                print(user)
+                yield user
