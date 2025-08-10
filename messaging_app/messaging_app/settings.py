28faefcd67394a02f86955b605e5638e2b802246
shemmee <56132945+s-shemmee@users.noreply.github.com>
2025-08-10 18:45:59 +0200

    Configure MySQL database settings, add Docker support, and update requirements

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE', 'messaging_db'),
+        'USER': os.environ.get('MYSQL_USER', 'messaging_user'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', 'messaging_pass'),
+        'HOST': os.environ.get('MYSQL_HOST', 'db'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
     }
 }
 
